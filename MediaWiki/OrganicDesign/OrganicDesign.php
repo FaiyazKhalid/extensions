12ec880d0f2bcf4d5f5bbb69a8a3e8f49321909e
Aran Dunkley <aran@organicdesign.co.nz>
2017-12-06 11:39:37 -0200

    bounce .co.nz

--- MediaWiki/OrganicDesign/OrganicDesign.php
+++ MediaWiki/OrganicDesign/OrganicDesign.php
@@ -53,6 +53,7 @@ class OrganicDesign {
 			$ssl = array_key_exists( 'HTTPS', $_SERVER ) && $_SERVER['HTTPS'] == 'on';
 			$od = preg_match( "|^www\.organicdesign\.(.+)$|", $host, $m );
 			$tld = $m[1] ? $m[1] : 'nz';
+			if( $tld == 'co.nz' ) $tld = 'nz';
 			if( !$od || !$ssl ) {
 				header( "Location: https://www.organicdesign.$tld$uri", true, 301 );
 				global $mediaWiki;
